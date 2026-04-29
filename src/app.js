0f795c4b5c8a800a2cd58b639af4d2babeadad65
Misskhat <misskhat.hossen@gmail.com>
2026-04-29 19:48:46 +0600

    cros origin and credential setup

--- src/app.js
+++ src/app.js
@@ -8,7 +8,12 @@ const bookingRouter = require('./routes/booking.routes')
 const paymentRouter = require('./routes/payment.routes')
 
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+)
 app.use(cookieParser())
 
 app.use('/api/services', serviceRouter)
