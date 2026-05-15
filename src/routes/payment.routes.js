cf2e5043138bf26b4607a3477cd87672278513d7
Md. Misskhat Hossen Milon <misskhat.hossen.com>
2026-05-15 10:58:20 +0600

    add Stripe payment intent route

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const { makePayment, allPayments, userAllPayments } = require('../services/payment.services')
-const { verifyToken } = require('../middleware/verifyToken')
-const { verifyAdmin } = require('../middleware/verifyAdmin')
-const router = express.Router()
+const express = require("express");
+const {
+  makePayment,
+  allPayments,
+  userAllPayments,
+  createPaymentIntent,
+} = require("../services/payment.services");
+const { verifyToken } = require("../middleware/verifyToken");
+const { verifyAdmin } = require("../middleware/verifyAdmin");
+const router = express.Router();
 
-router.post('/make-payment', verifyToken, makePayment)
-router.get('/all-payments', verifyToken, verifyAdmin, allPayments)
-router.get('/my-payments', verifyToken, userAllPayments)
+router.post("/make-payment", verifyToken, makePayment);
+router.get("/all-payments", verifyToken, verifyAdmin, allPayments);
+router.get("/my-payments", verifyToken, userAllPayments);
+router.post("/create-payment-intent", verifyToken, createPaymentIntent);
 
-module.exports = router
+module.exports = router;
--- src/services/payment.services.js
+++ src/services/payment.services.js
@@ -1,44 +1,63 @@
-const paymentModel = require('../models/payment.model')
+const paymentModel = require("../models/payment.model");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const makePayment = async (req, res) => {
-  const paymentInfo = req.body
+  const paymentInfo = req.body;
   try {
-    const payment = await paymentModel.create({ ...paymentInfo })
+    const payment = await paymentModel.create({ ...paymentInfo });
     res.status(200).json({
-      message: 'Payment successfully added',
+      message: "Payment successfully added",
       payment,
-    })
+    });
   } catch (err) {
-    console.log(err)
-    return res.status(500).json({ message: 'Something went wrong', err })
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong", err });
   }
-}
+};
 
 const allPayments = async (req, res) => {
   try {
-    const payments = await paymentModel.find()
+    const payments = await paymentModel.find();
     return res.status(200).json({
-      message: 'Successfully fetch all payment',
+      message: "Successfully fetch all payment",
       payments,
-    })
+    });
   } catch (err) {
-    console.log(err)
-    return res.status(500).json({ message: 'Something went wrong', err })
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong", err });
   }
-}
+};
 
 const userAllPayments = async (req, res) => {
   try {
-    const { email } = req.userInfo
-    const userAllPayments = await paymentModel.find({ userEmail: email })
+    const { email } = req.userInfo;
+    const userAllPayments = await paymentModel.find({ userEmail: email });
     return res.status(200).json({
-      message: 'Successfully fetch all payment',
+      message: "Successfully fetch all payment",
       payments: userAllPayments,
-    })
+    });
   } catch (err) {
-    console.log(err)
-    return res.status(500).json({ message: 'Something went wrong', err })
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong", err });
   }
-}
+};
 
-module.exports = { makePayment, allPayments, userAllPayments }
+const createPaymentIntent = async (req, res) => {
+  try {
+    const { amount } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100, // stripe uses cents
+      currency: "usd",
+    });
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong", err });
+  }
+};
+
+module.exports = {
+  makePayment,
+  allPayments,
+  userAllPayments,
+  createPaymentIntent,
+};
